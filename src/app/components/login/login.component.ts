7ec23c8e54c7139a7ecc52ccfbf2ad49a22b823f
Fernando Boza <hi@fernandoboza.com>
2019-09-26 21:10:34 -0400

    finished styling the login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,12 +9,18 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   public toggle: String = "login";
+  public terms_condition: boolean = false;
+  public term_modal: boolean = false;
 
   ngOnInit() {
   }
 
   public toggleViews(n: string) {
     this.toggle = n;
+  }
+
+  public showTerms() {
+    this.term_modal = !this.term_modal;
   }
 
 }
